445d24a61d5d9515cd951fbfbb34312dd063333c
wiredhorizon <wiredhorizon@forseti.dreamhost.com>
2014-01-19 03:21:36 -0800

    added some ground work

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <?php 
+  //define('DS', DIRECTORY_SEPARATOR);
+  define('DS', DIRECTORY_SEPARATOR);
+  define('ROOT', dirname(dirname(__FILE__)));
+  $url = $_GET['url'];
+
+  require_once (ROOT.DS.'library'.DS.'bootstrap.php');
+    
+
   function getContent ($path){
     if($file = file_get_contents($path)){
         return $file;
     } else {
-        return file_get_contents("error/404.html");
+        return file_get_contents(ROOT.DS."error/404.html");
     }
 
   }
@@ -15,10 +23,10 @@
   switch($path_array[1]){
     case '': 
       $title = $title." | Resume";
-      $content = getContent("partials/resume-body.html");
+      $content = getContent(ROOT.DS."partials/resume-body.html");
       break;
     case 'blog':
-      $content = getContent("partials/blog.html");
+      $content = getContent(ROOT.DS."partials/blog.html");
       break;
     default:
       //header('Location: http://wiredhorizon.com');
@@ -26,6 +34,7 @@
 ?>
 <html>
   <title><?echo $title?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/assets/stylesheets/bootstrap.min.css" rel="stylesheet"></link>
   <link href="/assets/stylesheets/stylesheet.css" rel='stylesheet'></link>
   <link href="/assets/stylesheets/stylesheet_print.css" rel='stylesheet' type="text/css" media="print"></link>
